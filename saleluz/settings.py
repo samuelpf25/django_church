0e7ff989cb0f1cdb6ee3e758b2bbf446ac2eee5f
samuel_uft <samuelpf25@gmail.com>
2022-06-20 07:59:51 -0300

    tentativa 2

--- saleluz/settings.py
+++ saleluz/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 # import django_heroku
 #
 # django_heroku.settings(locals())
@@ -77,11 +78,15 @@ WSGI_APPLICATION = 'saleluz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}"""
+
+DATABASES = {
+    'default': dj_database_url.config()
 }
 
 
@@ -129,3 +134,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGOUT_REDIRECT_URL = 'index'
